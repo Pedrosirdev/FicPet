5e4f02bf70dae70c988a1ca100ad09330393f80f
Pedro Henrique <pedrosirdev@gmail.com>
2023-08-12 00:51:14 -0300

    Finalização da pagina de adoção

--- src/app/components/AdoptionPage/AdoptionPage.jsx
+++ src/app/components/AdoptionPage/AdoptionPage.jsx
@@ -4,6 +4,9 @@ import AdoptionProfile from "../shared/AdoptionProfile/AdoptionProfile";
 import Title from "../shared/Title/Title";
 import WhiteCatAdoption from '../../../assets/images/white-cat-adoption.png';
 import Simba from '../../../assets/images/simba-adoption.png';
+import Max from '../../../assets/images/max-adoption.png';
+import Bella from '../../../assets/images/bella-adoption.png';
+
 function AdoptionPage(){
   return (
     <>
@@ -11,8 +14,10 @@ function AdoptionPage(){
     <section id="adoptionPage">
       <Title title="Pets disponíveis para adoção!" />
     <div className="cont-adoption-profile">
-      <AdoptionProfile classNameCardFooter="card-footer" namePet="Luna" descriptionPet="Elegante e brincalhona companheira felina." agePet="1 ano" classNameImg="img-card" className="card" src={WhiteCatAdoption} alt="Frederico para adoção" />
-      <AdoptionProfile classNameCardFooter="card-footer" namePet="Simba" classNameImg="img-card" className="card" src={Simba} alt="Frederico para adoção" />
+      <AdoptionProfile classNameCardFooter="card-footer" namePet="Luna" descriptionPet="Elegante e brincalhona companheira felina." agePet="1 ano de idade." classNameImg="img-card" className="card" src={WhiteCatAdoption} alt="Luna para adoção" />
+      <AdoptionProfile classNameCardFooter="card-footer" namePet="Simba" descriptionPet="Curioso e adorável explorador peludo." agePet="3 meses de idade." classNameImg="img-card" className="card" src={Simba} alt="Simba para adoção" />
+      <AdoptionProfile classNameCardFooter="card-footer" namePet="Max" descriptionPet="Leal e amoroso companheiro canino." agePet="1 ano de idade." classNameImg="img-card" className="card" src={Max} alt="Max para adoção" />
+      <AdoptionProfile classNameCardFooter="card-footer" namePet="Bella" descriptionPet="Doce e enérgica amiga de 4 patas." agePet="3 meses de idade." classNameImg="img-card" className="card" src={Bella} alt="Bella para adoção" />
     </div>
     </section>
     <Footer />
